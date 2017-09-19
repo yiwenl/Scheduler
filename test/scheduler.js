95b49d2862ca497b65926bd66668890182810f11
Yi-Wen Lin <bongiovi015@gmail.com>
2017-09-19 17:56:10 +0100

    Update build

--- test/scheduler.js
+++
@@ -1,1 +0,0 @@
-!function(r){function o(n){if(t[n])return t[n].exports;var e=t[n]={exports:{},id:n,loaded:!1};return r[n].call(e.exports,e,e.exports,o),e.loaded=!0,e.exports}var t={};return o.m=r,o.c=t,o.p="http://localhost:8080/",o(0)}([function(r,o,t){!function(){throw new Error('Cannot find module "./src/index.js"')}()}]);
